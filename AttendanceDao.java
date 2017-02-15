194dd28ff30eb5bf6fe06bf86fbe33438782eb9b
L00113302 <L00113302@student.lyit.ie>
2017-02-15 11:40:51 +0000

    Add files via upload

---
+++ AttendanceDao.java
@@ -0,0 +1,12 @@
+package AttendanceSystem;
+
+public interface AttendanceDao {
+	// persistent methods
+		public void add();
+		public void list();
+		public void view();
+		public void edit();
+		public void delete();
+		public void writeRecordsToFile();
+		public void readRecordsFromFile();
+}
---
+++ AttendanceMenu.java
@@ -0,0 +1,42 @@
+package AttendanceSystem;
+import java.util.Scanner;
+
+public class AttendanceMenu extends AttendanceFileHandler {
+	// instance variables
+	private int option;
+	Scanner keyboardIn = new Scanner(System.in);
+
+	// constructors
+	public AttendanceMenu() {
+		super();
+
+	}
+
+	// display menu for joint account
+	public void displayMenu() {
+		System.out.println("Joint Menu");
+		System.out.println("1 - Add");
+		System.out.println("2 - List");
+		System.out.println("3 - View");
+		System.out.println("4 - Edit");
+		System.out.println("5 - Delete");
+		System.out.println("6 - Quit");
+
+	}
+
+	// read option from menu
+	@SuppressWarnings("resource")
+	public void readOption() {
+
+		Scanner kbInt = new Scanner(System.in);
+		System.out.println("\n\tEnter Option [1|2|3|4|5|6]");
+		option = kbInt.nextInt();
+	}
+
+	// getter
+	public int getOption() {
+		return option;
+	}
+}
+	
+
