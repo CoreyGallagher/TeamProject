3caf26db3e3364c69ece3fc675de0dd6d573c827
CoreyGallagher <L00115921@student.lyit.ie>
2017-03-01 10:22:29 +0000

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,26 +1,71 @@
 package teamProject;
 
 
-public class Student extends User {
+public class Student  {
 	
 	//instance variables
+	String firstName , lastName, password, course;
+	//Module module;
+	//Class class;
+	int StudentID = 11001122;
 	
-	private int StudentID;
+	//constructor
+	public Student(String firstName, String lastName, String password){
+		
+		
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		StudentID ++;
+	}
+	// returns the students first name
+	public String getfirstName() {
+		return firstName;
+	}
+
+	// sets the students first name
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	// return the students ID
 	public int getStudentID() {
 		return StudentID;
 	}
 
-	public void setStudentID(int studentID) {
-		StudentID = studentID;
-	}
-
-	private static int nextNumber;
+	// Set the students ID
+	//public void setStudentID(int studentID) {
+	//	StudentID = studentID;
+	//}
 	
-	//constructor
-	public Student(String Name){
-		
-		super(Name);
-		StudentID = nextNumber;
+	//returns the studens last name
+	public String getLastName() {
+		return lastName;
 	}
+	
+	//sets the students last name
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	// return password
+	public String getPassword() {
+		return password;
+	}
+	
+	// set password
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	// toString() method
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + ", StudentID=" + StudentID + "]";
+	}
+	
+	
+	
+	
+	
 
 }
